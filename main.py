f6e7fe6d65ef3e2886bb15335dff47ace4d866f4
i d k ツ <92050717+ImWillIdk@users.noreply.github.com>
2023-03-19 11:27:10 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import requests
+import requests, os
 from bs4 import BeautifulSoup
 from time import sleep
-import os
 
-os.system('title idcum - Encrypter')
+os.system('@echo off')
+os.system('color c')
 
 print("""
                              ____  ___      __  ____   __ __  ____  ______    ___  ____  
@@ -17,82 +17,52 @@ print("""
                                                             
 """)
 
-print('\r                                                 |▌░░░░░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |█░░░░░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |█▌░░░░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |██░░░░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |██▌░░░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |███░░░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |███▌░░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |████░░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |████▌░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |█████░░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |█████▌░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |██████░░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |██████▌░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |███████░░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |███████▌░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |████████░░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |████████▌░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |█████████░░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |█████████▌░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |██████████░░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |██████████▌░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |███████████░░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |███████████▌░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |████████████░░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |████████████▌░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |█████████████░░░░|',end='')
-sleep(0.05)
-print('\r                                                 |█████████████▌░░░|',end='')
-sleep(0.05)
-print('\r                                                 |██████████████░░░|',end='')
-sleep(0.05)
-print('\r                                                 |██████████████▌░░|',end='')
-sleep(0.05)
-print('\r                                                 |███████████████░░|',end='')
-sleep(0.05)
-print('\r                                                 |███████████████▌░|',end='')
-sleep(0.05)
-print('\r                                                 |████████████████░|',end='')
-sleep(0.05)
-print('\r                                                 |████████████████▌|',end='')
-sleep(0.05)
-print('\r                                                 |█████████████████|',end='')
+os.system('title idcum - Encrypter')
+
+spacing = '                                                 '
+starting_message = '▌░░░░░░░░░░░░'
+lenght = len(starting_message)
+last_digit = starting_message[lenght-1]
+print(f'\r{spacing}|{starting_message}{last_digit}|',end='')
+position = starting_message.find('▌') + 1
+contra2 = False
+contra = False
+while position <= lenght + 1:
+    message = ''
+    b = 0
+    while b < position:
+
+        if contra == True and b == position - 1:
+            message = message + '▌'
+            contra = False
+            position = position - 1
+        elif contra == False and b == position - 1:
+            message = message + '█'
+            contra = True
+            contra2 = True
+        else:
+            message = message + '█'
+
+        b = b + 1
+
+    if contra2 == True and not position > lenght:
+        message = message + '░'
+        contra2 = False
+
+    final = lenght - position
+
+    for i in range(final):
+        message = message + '░'
+    position = position + 1
+    sleep(0.05)
+    print(f'\r{spacing}|{message}|',end='')
 
 url = 'https://superpatanegra.com/texto/index.php'
 
 enc_types = ['asc2bin', 'bin2asc', 'asc2hex', 'hex2asc', 'bin2hex', 'hex2bin', 'backwards', 'b64enc', 'b64dec', 'caesarbf', 'crypt', 'entityenc', 'entitydec', 'l33t', 'del33t', 'md5', 'igpay', 'unigpay', 'rot-13', 'urlenc', 'urldec']
 
 def __main__():
-
     os.system('cls')
-
     print(f"""
 
 
@@ -115,18 +85,15 @@ def __main__():
 
     number = int(input('[#] Elección: ')) - 1
 
-    text = input(f'Escriba el texto que desea encriptar/desencriptar con "{enc_types[number]}": ')
+    text = input(f'Escriba el texto que desea encriptar/desencriptar: ')
     r = requests.post(url, data={'text': text, 'cryptmethod': enc_types[number], 'submit': 'OK'})
     soup = BeautifulSoup(r.text, features='html.parser')
     data = [item.text for item in soup.select('p')]
-
-    mystring = data[1]
     keyword = 'TEXTO PROCESADO:'
-    before_keyword, keyword, after_keyword = mystring.partition(keyword)
-    encrypt = after_keyword
+    before_keyword, keyword, after_keyword = data[1].partition(keyword)
 
     print(f"""
-    {encrypt}
+    {after_keyword}
     """)
 
     input('Pulsa cualquier tecla para continuar: ')
