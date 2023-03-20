984f7bb62b6272fd1b23bfc2b3865a42b8d6fd46
i d k ツ <92050717+ImWillIdk@users.noreply.github.com>
2023-03-20 18:21:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from time import sleep
 from pystyle import Colors, Colorate, Write, Center
 
 os.system('@echo off')
-os.system('color c')
-os.system('title Encrypter / Decrypter - Disfunción Erectil')
+os.system('title  idcummm - Encrypter / Decrypter - Discord: lezduit#8218')
 
 print(Center.XCenter(Colorate.Vertical(Colors.yellow_to_red, """
 
@@ -24,46 +23,59 @@ print(Center.XCenter(Colorate.Vertical(Colors.yellow_to_red, """
 """, 1)))
 
 spacing = '                                                 '
-starting_message = '▌░░░░░░░░░░░░░░░░░'
-lenght = len(starting_message)
-last_char = starting_message[lenght-1]
-position = starting_message.find('▌') + 1
-contra2 = False
-contra = False
+lenght = 18
+position = 1
+help2 = False
+help = False
 
-print(Colors.orange, f'\r{spacing}|{starting_message}{last_char}|',end='')
+print(Colors.gray, f'\r{spacing}|░░░░░░░░░░░░░░░░░░░|',end='')
+print(Colors.gray, f'\r{spacing}|▌░░░░░░░░░░░░░░░░░░|',end='')
 
 while position <= lenght + 1:
     message = ''
     b = 0
 
     while b < position:
-        if contra == True and b == position - 1:
-            message = message + '▌'
+        if help == True and b == position - 1:
+            message += '▌'
 
-            contra = False
-            position = position - 1
-        elif contra == False and b == position - 1:
-            message = message + '█'
+            help = False
+            position -= 1
+        elif help == False and b == position - 1:
+            message += '█'
 
-            contra = True
-            contra2 = True
+            help = True
+            help2 = True
         else:
-            message = message + '█'
+            message += '█'
 
         b = b + 1
 
-    if contra2 == True and not position > lenght:
-        message = message + '░'
-        contra2 = False
+    if help2 == True and not position > lenght:
+        message += '░'
+        help2 = False
 
     for i in range(lenght - position):
-        message = message + '░'
-    position = position + 1
-    sleep(0.05)
-    print(Colors.orange, f'\r{spacing}|{message}|',end='')
+        message += '░'
+    position += 1
+    sleep(0.02)
+    print(Colors.gray, f'\r{spacing}|{message}|',end='')
 
 url = 'https://superpatanegra.com/texto/index.php'
+
+while True:
+    try:
+        requests.get(url)
+        print("""
+        """)
+        Write.Print('    >> [200] Conexión Establecida.', Colors.green, interval=0.01)
+        sleep(1)
+        break
+    except (ConnectionError, ConnectionAbortedError, ConnectionRefusedError, ConnectionResetError, Exception):
+        print("""
+        """)
+        Write.Print('    >> [404] Error de conexión. Reintentando Conexión en 10 segundos...', Colors.red, interval=0.01)
+        sleep(10)
 
 enc_types = ['asc2bin', 'bin2asc', 'asc2hex', 'hex2asc', 'bin2hex', 'hex2bin', 'backwards', 'b64enc', 'b64dec', 'caesarbf', 'crypt', 'entityenc', 'entitydec', 'l33t', 'del33t', 'md5', 'igpay', 'unigpay', 'rot-13', 'urlenc', 'urldec']
 
@@ -72,44 +84,52 @@ def __main__():
     print(Center.XCenter(f"""
 
 
-    {Colors.yellow}[{Colors.white}1{Colors.yellow}] {Colors.white}Texto a Binario                      {Colors.yellow}[{Colors.white}2{Colors.yellow}] {Colors.white}Binario a Texto               {Colors.yellow}[{Colors.white}3{Colors.yellow}] {Colors.white}Texto a Hexadecimal
+    {Colors.yellow}[{Colors.light_gray}1{Colors.yellow}] {Colors.white}Texto a Binario                      {Colors.yellow}[{Colors.light_gray}2{Colors.yellow}] {Colors.white}Binario a Texto               {Colors.yellow}[{Colors.light_gray}3{Colors.yellow}] {Colors.white}Texto a Hexadecimal
 
-    {Colors.yellow}[{Colors.white}4{Colors.yellow}] {Colors.white}Hexadecimal a Texto                  {Colors.yellow}[{Colors.white}5{Colors.yellow}] {Colors.white}Binario a Hexadecimal         {Colors.yellow}[{Colors.white}6{Colors.yellow}] {Colors.white}Hexadecimal a Binario
+    {Colors.yellow}[{Colors.light_gray}4{Colors.yellow}] {Colors.white}Hexadecimal a Texto                  {Colors.yellow}[{Colors.light_gray}5{Colors.yellow}] {Colors.white}Binario a Hexadecimal         {Colors.yellow}[{Colors.light_gray}6{Colors.yellow}] {Colors.white}Hexadecimal a Binario
 
-    {Colors.yellow}[{Colors.white}7{Colors.yellow}] {Colors.white}Texto al revés                       {Colors.yellow}[{Colors.white}8{Colors.yellow}] {Colors.white}Base 64 (codificar)           {Colors.yellow}[{Colors.white}9{Colors.yellow}] {Colors.white}Base 64 (descodificar)
+    {Colors.yellow}[{Colors.light_gray}7{Colors.yellow}] {Colors.white}Texto al revés                       {Colors.yellow}[{Colors.light_gray}8{Colors.yellow}] {Colors.white}Base 64 (codificar)           {Colors.yellow}[{Colors.light_gray}9{Colors.yellow}] {Colors.white}Base 64 (descodificar)
 
-    {Colors.yellow}[{Colors.white}10{Colors.yellow}] {Colors.light_red}Cifrado César                       {Colors.yellow}[{Colors.white}11{Colors.yellow}] {Colors.gray}Encriptación DES             {Colors.yellow}[{Colors.white}12{Colors.yellow}] {Colors.white}Entidades HTML (codificar)
+    {Colors.yellow}[{Colors.light_gray}10{Colors.yellow}] {Colors.light_red}Cifrado César                       {Colors.yellow}[{Colors.light_gray}11{Colors.yellow}] {Colors.gray}Encriptación DES             {Colors.yellow}[{Colors.light_gray}12{Colors.yellow}] {Colors.white}Entidades HTML (codificar)
 
-    {Colors.yellow}[{Colors.white}13{Colors.yellow}] {Colors.white}Entidades HTML (descodificar)       {Colors.yellow}[{Colors.white}14{Colors.yellow}] {Colors.white}Texto a l33t 5p34k           {Colors.yellow}[{Colors.white}15{Colors.yellow}] {Colors.white}l33t 5p34k a Texto
+    {Colors.yellow}[{Colors.light_gray}13{Colors.yellow}] {Colors.white}Entidades HTML (descodificar)       {Colors.yellow}[{Colors.light_gray}14{Colors.yellow}] {Colors.white}Texto a l33t 5p34k           {Colors.yellow}[{Colors.light_gray}15{Colors.yellow}] {Colors.white}l33t 5p34k a Texto
 
-    {Colors.yellow}[{Colors.white}16{Colors.yellow}] {Colors.gray}Encriptación MD5                    {Colors.yellow}[{Colors.white}17{Colors.yellow}] {Colors.white}Texto a Pig Latin            {Colors.yellow}[{Colors.white}18{Colors.yellow}] {Colors.white}Pig Latin a Texto
+    {Colors.yellow}[{Colors.light_gray}16{Colors.yellow}] {Colors.gray}Encriptación MD5                    {Colors.yellow}[{Colors.light_gray}17{Colors.yellow}] {Colors.white}Texto a Pig Latin            {Colors.yellow}[{Colors.light_gray}18{Colors.yellow}] {Colors.white}Pig Latin a Texto
 
-    {Colors.yellow}[{Colors.white}19{Colors.yellow}] {Colors.white}Texto a ROT-13                      {Colors.yellow}[{Colors.white}20{Colors.yellow}] {Colors.white}Codificar URL                {Colors.yellow}[{Colors.white}21{Colors.yellow}] {Colors.white}Descodificar URL
+    {Colors.yellow}[{Colors.light_gray}19{Colors.yellow}] {Colors.white}Texto a ROT-13                      {Colors.yellow}[{Colors.light_gray}20{Colors.yellow}] {Colors.white}Codificar URL                {Colors.yellow}[{Colors.light_gray}21{Colors.yellow}] {Colors.white}Descodificar URL
 
 
     """))
     while True:
         try:
             number = int(Write.Input("    [#] Elección: ", Colors.light_gray, interval=0.01)) - 1
+            print()
             break
         except (TypeError, ValueError):
+            print()
             Write.Print('    Porvafor, escriba un número válido.', Colors.light_gray, interval=0.01)
+            print("""
+            """)
             sleep(0.5)
 
     if number == 9:
         Write.Print('    En desarrollo... ', Colors.light_gray, interval=0.01)
+        print()
     else:
         while True:
             try:
                 text = Write.Input('    Escriba el mensaje que desea encriptar/desencriptar: ', Colors.light_gray, interval=0.01)
-                r = requests.post(url, data={'text': text, 'cryptmethod': enc_types[number], 'submit': 'OK'})
+                r = requests.post(url, data={'text': text, 'cryptmethod': enc_types[number], 'submit': 'OK'})       
                 soup = BeautifulSoup(r.text, features='html.parser')
                 data = [item.text for item in soup.select('p')]
                 keyword = 'TEXTO PROCESADO:'
                 before_keyword, keyword, after_keyword = data[1].partition(keyword)
                 break
             except IndexError:
+                print()
                 Write.Print('    Porvafor, escriba un mensaje válido.', Colors.light_gray, interval=0.01)
+                print("""
+                """)
                 sleep(0.5)
         if len(after_keyword) > 200:
            print(Colors.yellow, f"""
@@ -119,9 +139,9 @@ def __main__():
         else:
             Write.Print(f"""
             {after_keyword}
-        
             """, Colors.yellow, interval=0.001)
 
+    print()
     Write.Input('    Pulsa cualquier tecla para continuar: ', Colors.light_gray, interval=0.01)
 
 while True:
