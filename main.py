5a7916a57097f6289b51ef0f86f940dcb9afe3d6
i d k ツ <92050717+ImWillIdk@users.noreply.github.com>
2023-03-24 23:55:19 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ url = 'https://superpatanegra.com/texto/index.php'
 enc_types = ['asc2bin', 'bin2asc', 'asc2hex', 'hex2asc', 'bin2hex', 'hex2bin', 'backwards', 'b64enc', 'b64dec', 'caesarbf', 'crypt', 'entityenc', 'entitydec', 'l33t', 'del33t', 'md5', 'igpay', 'unigpay', 'rot-13', 'urlenc', 'urldec']
 after_keyword = ''
 
+print('\n')
+Write.Print(f'    >> Intentando establecer la conexión con el servidor...', Colors.white, interval=0.01)
 while True:
     try:
         requests.get(url)
-        print('\n')
-        Write.Print(f'    >> [{requests.get(url).status_code}] Conexión Establecida.', Colors.green, interval=0.01)
+        print()
+        Write.Print(f'    >> [{requests.get(url).status_code}] Conexión establecida.', Colors.green, interval=0.01)
         sleep(1)
         break
     except (ConnectionError, ConnectionAbortedError, ConnectionRefusedError, ConnectionResetError, Exception):
@@ -23,40 +25,42 @@ while True:
 clear()
 enc_options()
 def __main__():
-    while True:
-        try:
-            number = int(Write.Input("    >> [#] Elección: ", Colors.light_gray, interval=0.01)) - 1
-            print()
-            break
-        except (TypeError, ValueError):
-            print()
-            Write.Print('    >> Porvafor, escriba un número válido.', Colors.light_red, interval=0.01)
-            print('\n')
-            sleep(0.5)
+    try:
+        number = int(Write.Input("    >> [#] Elección: ", Colors.light_gray, interval=0.01)) - 1
+        print()
+    except (TypeError, ValueError):
+        number = 1000
+        print()
 
+    global after_keyword
     if number == 9:
         while True:
             try:
                 text = Write.Input('    >> Escriba el mensaje que desea encriptar/desencriptar: ', Colors.light_gray, interval=0.01)
                 if text == '':
-                    global after_keyword
                     text = after_keyword
                 r = requests.post(url, data={'text': text, 'cryptmethod': enc_types[number], 'submit': 'OK'})       
                 soup = BeautifulSoup(r.text, features='html.parser')
                 data = [item.text for item in soup.select('td')]
+                keyword = ': '
+                before_keyword, keyword, after_keyword = data[1].partition(keyword)
                 break
             except IndexError:
                 print()
-                print(data)
                 Write.Print('    >> Porvafor, escriba un mensaje válido.', Colors.light_red, interval=0.01)
                 print('\n')
                 sleep(0.5)
-            print(Colors.yellow, f"""
-            {data}
-            """)
-    elif number > 20 or number < -1:
-        Write.Print('    >> En desarrollo... ', Colors.light_red, interval=0.01)
         print()
+        for i in range(len(data)):
+            keyword = ': '
+            before_keyword, keyword, after_keyword = data[i].partition(keyword)
+            print(Colors.yellow, f'{after_keyword}')
+        print()
+    elif number > 20 or number < -1:
+        print()
+        Write.Print('    >> Porvafor, escriba un número válido.', Colors.light_red, interval=0.01)
+        print('\n')
+        sleep(0.5)
     elif number == -1:
         clear()
         config_options()
