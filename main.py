a324e5fd19099db8805554c17534ec613acbdc6f
ImWillIdk <devesaguillem@gmail.com>
2023-03-26 01:16:22 +0100

    update

--- main.py
+++ main.py
@@ -5,28 +5,36 @@ idcrypter()
 progress_bar(30, 0, 0.03)
 
 url = 'https://superpatanegra.com/texto/index.php'
-enc_types = ['asc2bin', 'bin2asc', 'asc2hex', 'hex2asc', 'bin2hex', 'hex2bin', 'backwards', 'b64enc', 'b64dec', 'caesarbf', 'crypt', 'entityenc', 'entitydec', 'l33t', 'del33t', 'md5', 'igpay', 'unigpay', 'rot-13', 'urlenc', 'urldec']
+url2 = 'https://cifraronline.com/descifrar-aes'
+enc_types = ['asc2bin', 'bin2asc', 'asc2hex', 'hex2asc', 'bin2hex', 'hex2bin', 'backwards', 'b64enc', 'b64dec',
+             'caesarbf', 'crypt', 'entityenc', 'entitydec', 'l33t', 'del33t', 'md5', 'igpay', 'unigpay', 'rot-13', 'urlenc', 'urldec']
 after_keyword = ''
 
 print('\n')
-Write.Print(f'    >> Intentando establecer la conexión con el servidor...', Colors.white, interval=0.01)
+Write.Print(f'    >> Intentando establecer la conexión con el servidor...',
+            Colors.white, interval=0.01)
 while True:
     try:
         requests.get(url)
         print()
-        Write.Print(f'    >> [{requests.get(url).status_code}] Conexión establecida.', Colors.green, interval=0.01)
+        Write.Print(
+            f'    >> [{requests.get(url).status_code}] Conexión establecida.', Colors.green, interval=0.01)
         sleep(1)
         break
     except (ConnectionError, ConnectionAbortedError, ConnectionRefusedError, ConnectionResetError, Exception):
         print('\n')
-        Write.Print('    >> [404] Error de conexión. Reintentando Conexión en 10 segundos...', Colors.red, interval=0.01)
+        Write.Print(
+            '    >> [404] Error de conexión. Reintentando Conexión en 10 segundos...', Colors.red, interval=0.01)
         sleep(10)
 
 clear()
 enc_options()
+
+
 def __main__():
     try:
-        number = int(Write.Input("    >> [#] Elección: ", Colors.light_gray, interval=0.01)) - 1
+        number = int(Write.Input(
+            "    >> [#] Elección: ", Colors.light_gray, interval=0.01)) - 1
         print()
     except (TypeError, ValueError):
         number = 1000
@@ -36,19 +44,23 @@ def __main__():
     if number == 9:
         while True:
             try:
-                text = Write.Input('    >> Escriba el mensaje que desea encriptar/desencriptar: ', Colors.light_gray, interval=0.01)
+                text = Write.Input(
+                    '    >> Escriba el mensaje que desea encriptar/desencriptar: ', Colors.light_gray, interval=0.01)
                 print()
                 if text == '':
                     text = after_keyword
-                r = requests.post(url, data={'text': text, 'cryptmethod': enc_types[number], 'submit': 'OK'})       
+                r = requests.post(
+                    url, data={'text': text, 'cryptmethod': enc_types[number], 'submit': 'OK'})
                 soup = BeautifulSoup(r.text, features='html.parser')
                 data = [item.text for item in soup.select('td')]
                 keyword = ': '
-                before_keyword, keyword, after_keyword = data[1].partition(keyword)
+                before_keyword, keyword, after_keyword = data[1].partition(
+                    keyword)
                 print()
                 break
             except IndexError:
-                Write.Print('    >> Porvafor, escriba un mensaje válido.', Colors.light_red, interval=0.01)
+                Write.Print('    >> Porvafor, escriba un mensaje válido.',
+                            Colors.light_red, interval=0.01)
                 print('\n')
                 sleep(0.5)
         for i in range(len(data)):
@@ -56,42 +68,49 @@ def __main__():
             before_keyword, keyword, after_keyword = data[i].partition(keyword)
             print(Colors.yellow, f'[+{i+1}]:\t\t{after_keyword}')
         print()
-    elif number > 20 or number < -1:
-        Write.Print('    >> Porvafor, escriba un número válido.', Colors.light_red, interval=0.01)
-        print()
-        sleep(0.5)
     elif number == -1:
         clear()
         config_options()
         config_main()
-    else:
+    elif number < 21 and number > -1:
         while True:
             try:
-                text = Write.Input('    >> Escriba el mensaje que desea encriptar/desencriptar: ', Colors.light_gray, interval=0.01)
+                text = Write.Input(
+                    '    >> Escriba el mensaje que desea encriptar/desencriptar: ', Colors.light_gray, interval=0.01)
                 print()
                 if text == '':
                     text = after_keyword
-                r = requests.post(url, data={'text': text, 'cryptmethod': enc_types[number], 'submit': 'OK'})       
+                r = requests.post(
+                    url, data={'text': text, 'cryptmethod': enc_types[number], 'submit': 'OK'})
                 soup = BeautifulSoup(r.text, features='html.parser')
                 data = [item.text for item in soup.select('p')]
                 keyword = 'TEXTO PROCESADO:'
-                before_keyword, keyword, after_keyword = data[1].partition(keyword)
+                before_keyword, keyword, after_keyword = data[1].partition(
+                    keyword)
                 break
             except IndexError:
-                Write.Print('    >> Porvafor, escriba un mensaje válido.', Colors.light_red, interval=0.01)
+                Write.Print('    >> Porvafor, escriba un mensaje válido.',
+                            Colors.light_red, interval=0.01)
                 print('\n')
                 sleep(0.5)
         if len(after_keyword) > 200:
-           print(Colors.yellow, f'\n{after_keyword}\n\n')
+            print(Colors.yellow, f'\n{after_keyword}\n\n')
         else:
             Write.Print(f"""
-            {after_keyword}
-            """, Colors.yellow, interval=0.001)
+                        {after_keyword}
+                        """, Colors.yellow, interval=0.001)
+    else:
+        Write.Print('    >> Porvafor, escriba un número válido.',
+                    Colors.light_red, interval=0.01)
+        print()
+        sleep(0.5)
 
     print()
-    Write.Input('    >> Pulsa cualquier tecla para continuar: ', Colors.light_gray, interval=0.01)
+    Write.Input('    >> Pulsa cualquier tecla para continuar: ',
+                Colors.light_gray, interval=0.01)
     clear()
     enc_options()
+
 
 while True:
     __main__()
